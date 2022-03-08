473c3cf9047e8c5a08ba24ca7b6672232d71b993
Joe Giuffrida <conquercat24@gmail.com>
2022-03-07 16:21:48 -0800

    added error handling for createCoffeeStore api if id or name are empty it will result in status 400 with error message saying either field is missing

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -12,45 +12,57 @@ const createCoffeeStoreHandler = async (req, res) => {
     const { id, name, address, neighborhood, imgUrl, votes } = req.body;
 
     try {
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id=${id}`,
-        })
-        .firstPage();
+      // if (id){} is validation saying we need id field in order to proceed
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id=${id}`,
+          })
+          .firstPage();
 
-      if (findCoffeeStoreRecords.length !== 0) {
-        // we're mapping over findCoffeeStoreRecords because we only want the fields portion of the data which has our record info
-        const record = findCoffeeStoreRecords.map((storeRecord) => {
-          return {
-            ...storeRecord.fields,
-          };
-        });
-        res.json(record);
+        if (findCoffeeStoreRecords.length !== 0) {
+          // we're mapping over findCoffeeStoreRecords because we only want the fields portion of the data which has our record info
+          const record = findCoffeeStoreRecords.map((storeRecord) => {
+            return {
+              ...storeRecord.fields,
+            };
+          });
+          res.json(record);
+        } else {
+          // create a record
+          // if (name){} is validation saying we need name field in order to proceed
+          if (id && name) {
+            const createRecord = await table.create([
+              {
+                fields: {
+                  id: id,
+                  name: name,
+                  address: address,
+                  neighborhood: neighborhood,
+                  imgUrl: imgUrl,
+                  votes: votes,
+                },
+              },
+            ]);
+            // transforming the createRecord data to only include the fields data which has our record data that we need and storing it in createdRecord
+            const createdRecord = createRecord.map((storeRecord) => {
+              return {
+                ...storeRecord.fields,
+              };
+            });
+            res.json(createdRecord);
+          } else {
+            res.status(400).json({ message: `name is missing` });
+          }
+        }
       } else {
-        // create a record
-        const createRecord = await table.create([
-          {
-            fields: {
-              id: id,
-              name: name,
-              address: address,
-              neighborhood: neighborhood,
-              imgUrl: imgUrl,
-              votes: votes,
-            },
-          },
-        ]);
-        // transforming the createRecord data to only include the fields data which has our record data that we need and storing it in createdRecord
-        const createdRecord = createRecord.map((storeRecord) => {
-          return {
-            ...storeRecord.fields,
-          };
-        });
-        res.json(createdRecord);
+        res.status(400).json({ message: `id is missing` });
       }
     } catch (error) {
       console.error(`Error at createCoffeeStoreHandler: ${error}`);
-      res.status(500).json({ message: `Error finding store: ${error}` });
+      res
+        .status(500)
+        .json({ message: `Error finding or creating store: ${error}` });
     }
   }
 };
